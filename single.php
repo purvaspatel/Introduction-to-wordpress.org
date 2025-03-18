e4b2c7b1191c507b310c4e117562f64cf1f7bc8e
unknown <purvaspatel1241@gmail.com>
2025-03-18 11:19:30 +0530

    setup individual post page

--- single.php
+++ single.php
@@ -3,9 +3,29 @@
 
 while(have_posts()){
     the_post();?>
-    <h2><?php the_title();?></h2>
-    <p><?php the_content();?></p>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg');?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title();  ?></h1>
+        <div class="page-banner__intro">
+          <p>THIS IS SUPPOSED TO BE REPLACED LATER</p>
+        </div>
+      </div>
+    </div>
+
     
+ <div class="container container--narrow page-section">
+ <div class="metabox metabox--position-up metabox--with-home-link">
+            <p>
+              <a class="metabox__blog-home-link" href="<?php echo site_url("/blogs");?>"><i class="fa fa-home" aria-hidden="true"></i> Back to Blogs</a> <span class="metabox__main"><?php the_title();?></span>
+            </p>
+         </div>
+    <div class="generic-content">
+    <?php the_content(); ?>
+
+    </div>
+</div>
+
     <?php 
 
 }
