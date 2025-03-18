809eb3d1605edb83af616dae78010dba8ea39a9f
unknown <purvaspatel1241@gmail.com>
2025-03-18 11:11:23 +0530

    setup pages to view all posts/blogs

--- index.php
+++ index.php
@@ -12,6 +12,28 @@ get_header();
       </div>
     </div>
 
+<div class="container container--narrow page-section">
+  <?php 
+    while(have_posts()){
+      the_post();?>
+      <div class="post-item">
+        <h2 class="headline headline--medium headline-post-title"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+
+    <div class="metabox">
+      <p>Created by <?php the_author_posts_link();?> on <?php the_time();?> in News</p>
+    </div>
+
+    <div class="generic-content"></div>
+    <?php 
+    the_excerpt();
+    ?>
+    <p><a href="<?php the_permalink(); ?>">Continue Reading &raquo;</a></p>
+    </div>
+  <?php  }
+  ?>
+</div>
+
+
 <?php
 get_footer();
 
